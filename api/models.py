b27b78dd9237b7c548d673cb42b60cd0a6be8090
alquis-facio <jenych0314@naver.com>
2025-11-18 15:24:27 +0900

    feat: 모델 type 수정 및 pins 모델 추가

--- api/models.py
+++ api/models.py
@@ -7,14 +7,25 @@ from database import Base
 
 # SQLAlchemy Model
 class User(Base):
-    __tablename__ = "users"  # 'user'는 예약어 충돌 위험 (Postgres 등)
+    __tablename__ = "users"
 
-    user_id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    email: Mapped[str] = mapped_column(String(255), unique=True, index=True, nullable=False)
-    password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
-    name: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
-    username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
-    image: str = ""
-    refresh_token: str = ""
-    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
-    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    user_id: Mapped[int]            = mapped_column(Integer,        primary_key=True, index=True)
+    email: Mapped[str]              = mapped_column(String(255),    unique=True, index=True, nullable=False)
+    password_hash: Mapped[str]      = mapped_column(String(255),    nullable=False)
+    name: Mapped[str]               = mapped_column(String(50),     unique=True, nullable=False)
+    username: Mapped[str]           = mapped_column(String(50),     unique=True, nullable=False)
+    image: Mapped[str]
+    refresh_token: Mapped[str]
+    created_at: Mapped[datetime]    = mapped_column(DateTime(timezone=True), server_default=func.now())
+    updated_at: Mapped[datetime]    = mapped_column(DateTime(timezone=True), server_default=func.now())
+
+class Pin(Base):
+    __tablename__ = "pins"
+
+    pin_id: Mapped[int]             = mapped_column(Integer,        primary_key=True, index=True)
+    user_id: Mapped[int]            = mapped_column(Integer, ForeignKey('users.user_id', ondelete="CASCADE"))
+    title: Mapped[str]              = mapped_column(String(255),    nullable=False)
+    content: Mapped[str]            = mapped_column(String(255))
+    image: Mapped[str]              = mapped_column(String(255),    nullable=False)
+    created_at: Mapped[datetime]    = mapped_column(DateTime(timezone=True), server_default=func.now())
+    updated_at: Mapped[datetime]    = mapped_column(DateTime(timezone=True), server_default=func.now())
