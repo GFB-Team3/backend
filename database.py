55ad89fd281d80bdb49a0a8fcce8586a49dda6af
alquis-facio <jenych0314@naver.com>
2025-11-18 15:25:12 +0900

    feat: 세션 관리 함수 수정

--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 from typing import Generator
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, Session
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./pinter5t.db"
 
@@ -10,19 +9,21 @@ engine = create_engine(
     connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(
-    autoflush=False,
-    autocommit=False,
+    autoflush=False, # 쿼리를 날리기 전 자동 flush X
+    autocommit=False, # 명시적으로 db.commit()을 해야 함
     bind=engine
 )
 
-Base = declarative_base()
-
 class Base(DeclarativeBase):
+    """모든 ORM 모델이 상속할 기본 Base 클래스"""
     pass
 
-def get_db() -> Generator:
-    db = SessionLocal()
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal() # 세션 생성
     try:
         yield db # 함수에 DB 세션 전달
+    except:
+        db.rollback()
+        raise
     finally:
         db.close() # db 닫기
